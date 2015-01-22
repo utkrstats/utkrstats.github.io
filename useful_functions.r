106c1a6ae78b202b3c28423c75baaefdb598e55a
utkrstats <utkrstats@gmail.com>
2015-01-22 12:58:48 -0800

    Create useful_functions.r

---
+++ useful_functions.r
@@ -0,0 +1,11 @@
+# the following are examples of some very common and very useful functions in R
+
+# generate a sequence
+my_seq <- seq(1,100)
+
+# find the sum of a vector
+v <- c(1:10)
+sum(v)
+
+
+
