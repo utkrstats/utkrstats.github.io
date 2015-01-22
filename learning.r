f32d02e0e92f0856a3230f7377ad62d34f77fc9b
utkrstats <utkrstats@gmail.com>
2015-01-22 11:16:04 -0800

    Update learning.r

--- learning.r
+++ learning.r
@@ -9,3 +9,10 @@
 22/17 + 37/47 + 88/83
 
 
+# you can also store numbers as names so you don't have to type them again
+
+favorite_num <- 3.14159265358979323846264338327950288419716939937510582097
+
+
+
+
