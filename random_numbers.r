9a2f98f5d2d2100b9fc6709f07ce35ad692b2966
utkrstats <utkrstats@gmail.com>
2015-01-22 23:34:55 -0500

    Update random_numbers.r

--- random_numbers.r
+++ random_numbers.r
@@ -7,6 +7,9 @@ hist(x)
 
 ### what if you wanted 1000 numbers? what about 10,000?
 
+# another very common one is the uniform distribution
+my_unif <- runif(100)
+hist(my_unif)
 
 # the poisson distribution is very common for ...
 # the following gives you 100 random numbers from the poisson distribution (lambda = 3)
@@ -14,3 +17,13 @@ my_pois <- rpois(100,3)
 hist(my_pois)
 
 
+# plotting densities is nice to give you another look at the shape of the distribution
+# compare
+
+hist(x)
+
+# and
+
+plot(density(x))
+
+# what do you see?
