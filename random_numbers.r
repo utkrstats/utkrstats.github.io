e167bc5cd3806d9df087752f68f4a5a552226aa8
utkrstats <utkrstats@gmail.com>
2015-01-22 22:36:14 -0500

    Create random_numbers.r

---
+++ random_numbers.r
@@ -0,0 +1,9 @@
+# random numbers are super super super useful
+# and R has a bunch of very quick ways to get them
+
+# gives you 100 random numbers from the normal distribution ("bell curve")
+x <- rnorm(100)
+hist(x)
+
+### what if you wanted 1000 numbers? what about 10,000?
+
