e157a52618fdc385894c86b0a62cc7c0d022dd6e
utkrstats <utkrstats@gmail.com>
2015-01-22 22:14:33 -0500

    Create base_plots.r

---
+++ base_plots.r
@@ -0,0 +1,7 @@
+# R has a really powerful base plotting system.
+# We're going to go over the most basic of these options
+# In a later session, we'll go over another way to do it
+
+#generate some random data to plot
+x <- rnorm(100)
+y <- rlnorm(100)
