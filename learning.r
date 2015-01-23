1459e7870687769dd8a4e62848e77eb0ef6dc78c
utkrstats <utkrstats@gmail.com>
2015-01-22 22:46:20 -0500

    Update learning.r

--- learning.r
+++ learning.r
@@ -1,7 +1,6 @@
 # R is a pretty awesome calculator
 
 # put your cursor over the appropriate line and press ctrl+r to run
-
 1 + 1
 
 1 / 3
@@ -10,7 +9,6 @@
 
 
 # you can also store numbers as names so you don't have to type them again
-
 favorite_num <- 3.14159265358979323846264338327950288419716939937510582097
 
 second_favorite_num <- favorite_num * 2
@@ -20,6 +18,17 @@ pi
 
 
 # you can also get a bunch of numbers really easily
-
 nums <- c(1:100)
+
+# R is like a lego castle. It looks very complicated but is actually made of small blocks
+# the class() function tells you what these blocks are called
+
+class(1) # what is a 1?
+
+class("1")
+
+class(TRUE)
+
+class(class)
+
 
