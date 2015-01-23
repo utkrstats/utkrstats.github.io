77319e97aece6c993c7c45cb49b8eb507235b4f4
utkrstats <utkrstats@gmail.com>
2015-01-22 22:03:32 -0500

    Update useful_functions.r

--- useful_functions.r
+++ useful_functions.r
@@ -3,13 +3,28 @@
 # generate a sequence
 my_seq <- seq(1,100)
 
+by_twos <- seq(1,100,2)
+
 # find the sum of a vector
 v <- c(1:10)
 sum(v)
+
+# average of a vector
+mean(my_seq)
+
+# standard deviation
+sd(my_seq)
+
+# biggest number in a vector
+max(my_seq)
+
+# and the smallest
+min(my_seq)
 
 # how long is the vector?
 vect <- which(runif(100)>.4)
 length(vect)
 
-
+# better yet, find out a bunch of information
+summary(my_seq)
 
