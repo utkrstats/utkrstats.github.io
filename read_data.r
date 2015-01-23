07b3f53636e9a47c6491a113732e286f5b97ba99
utkrstats <utkrstats@gmail.com>
2015-01-22 22:52:43 -0500

    Update read_data.r

--- read_data.r
+++ read_data.r
@@ -1,3 +1,11 @@
 # we will go over the read.csv function, which you will use a lot
 
 my_data <- read.csv("YOUR FILE HERE")
+
+  # if your file doesn't have headers
+  my_data <- read.csv("YOUR FILE HERE", header=FALSE)
+
+# just for fun...
+class(my_data)
+
+
