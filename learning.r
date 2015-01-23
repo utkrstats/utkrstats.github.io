d2463904769d3a3d2156bb53dcf086ca55dc1d93
utkrstats <utkrstats@gmail.com>
2015-01-22 22:38:36 -0500

    Update learning.r

--- learning.r
+++ learning.r
@@ -15,6 +15,9 @@ favorite_num <- 3.14159265358979323846264338327950288419716939937510582097
 
 second_favorite_num <- favorite_num * 2
 
+# R is nice, though, and it already has pi as a variable
+pi
+
 
 # you can also get a bunch of numbers really easily
 
