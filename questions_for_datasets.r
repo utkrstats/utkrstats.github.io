1bee00afd22041980963bd9e7d545e8cc34a3c03
utkrstats <utkrstats@gmail.com>
2015-01-24 13:20:15 -0500

    Create questions_for_datasets.r

---
+++ questions_for_datasets.r
@@ -0,0 +1,18 @@
+# here are some common things you can "ask" R to tell you about your plethora of datasets
+
+my_data <- read.csv("YOURFILE")
+
+# what are the first few rows?
+head(my_data)
+
+# what are the dimensions?
+dim(my_data)
+
+  # or
+  nrow(my_data)
+  ncol(my_data)
+
+# what are the column names?
+names(my_data)
+
+
