6ee65d14dafc9ff17734630e861392828c44d070
utkrstats <utkrstats@gmail.com>
2015-01-26 06:22:20 -0800

    Create user_defined_functions.r

---
+++ user_defined_functions.r
@@ -0,0 +1,14 @@
+# much like the sum() or log()
+# R will allow you to make your own functions
+
+# this is really helpful so that you don't repeat yourself when writing code
+
+# here is a template function
+
+my_function <- function(my_arguments) {
+
+  return(my_arguments)
+
+}
+
+# this function does nothing meaningful
